299edfcc3105fd594374d3b9f4d97d708d50c33a
zionmushussu <zionctech44719@gmail.com>
2022-01-14 01:57:09 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
+
+
+// <!-- Read the Formbutton docs at formspree.io/formbutton/docs. See more examples at codepen.io/formspree -->
+// <scrip src="https://formspree.io/js/formbutton-v1.min.js" defer></script>
+
+/* paste this line in verbatim */
+window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+/* customize formbutton below*/     
+formbutton("create", {
+  action: "https://formspree.io/f/mdobobzv",
+  title: "How can we help?",
+  fields: [
+    { 
+      type: "email", 
+      label: "Email:", 
+      name: "email",
+      required: true,
+      placeholder: "your@email.com"
+      },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+      placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "gray"
+      },
+      button: {
+      backgroundColor: "gray"
+    }
+  }
+});
